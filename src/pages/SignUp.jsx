653531958c7f979bca13d063562c0d2e06bca063
Melanie Brown <mbrown9702@gmail.com>
2025-09-02 19:54:12 -0500

    working

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import supabase from '@/lib/supabase'
 import { Link, useNavigate, useLocation } from "react-router-dom";
-import logo from "../assets/logo.png";
+import "/logo.png";
 
 export default function SignUp() {
   const [email, setEmail] = useState("");
